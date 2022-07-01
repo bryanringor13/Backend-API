4615f37744b5ec1694d268b345ac0af871b89ff9
bryan <bryan.ringor@gmail.com>
2022-07-01 11:10:27 +0800

    mongo add prod config

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,7 +1,8 @@
+const envronment = process.env.NODE_ENV === 'development' ? 'mongodb://' : 'mongodb+srv://';
 
 export const mongoUrl = (envServer, db) => {
     const servers = envServer ? envServer.split(' ') : ['localhost:27017']
-    const url = servers.reduce((prev, cur) => prev + cur + ',', 'mongodb://')
+    const url = servers.reduce((prev, cur) => prev + cur + ',', envronment);
 
     return `${url.substr(0, url.length - 1)}/${db}`
 }
